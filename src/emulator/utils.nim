e8696434ca0f45a296c0556b9eeecf53ecf7179a
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-09-07 12:32:20 +0530

    fix: decrease range for opcode.y and opcode.z

--- src/emulator/utils.nim
+++ src/emulator/utils.nim
@@ -3,15 +3,15 @@ import types
 
 type 
     u2 = range[0'u8..3'u8]
-    u4 = range[0'u8..15'u8]
+    u3 = range[0'u8..7'u8]
 
 func lsb*(value: uint16): uint8 {.inline.} = (value and 0xFF).uint8
 func msb*(value: uint16): uint8 {.inline.} = (value shr 8).uint8
 func concat*(lo: uint8, hi: uint16): uint16 {.inline.} = (hi shl 8) or lo
 
 func x*(opcode: uint8): u2 {.inline.} = opcode.bitsliced(6..7)
-func y*(opcode: uint8): u4 {.inline.} = opcode.bitsliced(3..5)
-func z*(opcode: uint8): u4 {.inline.} = opcode.bitsliced(0..2)
+func y*(opcode: uint8): u3 {.inline.} = opcode.bitsliced(3..5)
+func z*(opcode: uint8): u3 {.inline.} = opcode.bitsliced(0..2)
 func p*(opcode: uint8): u2 {.inline.} = opcode.bitsliced(4..5)
 func q*(opcode: uint8): bool {.inline.} = opcode.testbit(3).bool
 
