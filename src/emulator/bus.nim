28a3a89eadad62cc695ee01905caba277a062e4d
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2024-03-15 21:31:56 +0530

    feat: add PPU Registers, DMA and graphics bus

--- src/emulator/bus.nim
+++ src/emulator/bus.nim
@@ -1,23 +1,75 @@
+import io
 import cartridge
 import utils
 import timer
-import io
+import graphics/bus
+
+type
+    DMA* = ref object
+        active*: bool
+        currentByte: uint8
+        currentIndex: uint8
+        starting: bool
+        initialDelay: uint8
+
+proc tick(d: DMA): void
+
 
 var
     rom*: ROM
-    wram*: array[0x2000, uint8]
-    hram*: array[0x80, uint8]
+    wram: array[0x2000, uint8]
+    hram: array[0x80, uint8]
+
+    # TODO: add support for external ram
+    mockExternalRam: array[0x2000, uint8]
+
+    dma* = DMA()
 
 
-proc readByte*(address: uint16, incr = true): uint8 =
+template cycle(incr: bool = true): void =
+    if not incr:
+        return
+
+    if dma.active:
+        dma.tick()
+
+    incCycle(1)
+
+    if dma.starting:
+        if dma.initialDelay > 1:
+            dma.initialDelay -= 1
+
+        else:
+            dma.active = true
+
+proc readByte*(address: uint16, incr = true, conflict = true): uint8 =
+    if dma.active and (not conflict) and address.isboundto(0xFE00, 0xFFFF):
+        result = wram[(address - 0xFE00) + 0x1E00]
+        return
+
     if address.isboundto(0, 0x7FFF):
         result = rom.read(address)
+
+    elif address.isboundto(0x8000, 0x9FFF):
+        result = bus.readByte(address)
+
+    elif address.isboundto(0xA000, 0xBFFF):
+        result = mockExternalRam[address - 0xA000]
 
     elif address.isboundto(0xC000, 0xDFFF):
         result = wram[address - 0xC000]
 
+    elif address.isboundto(0xE000, 0xFDFF): # mirror
+        result = wram[address - 0xE000]
+
+    elif address.isboundto(0xFE00, 0xFE9F):
+        if dma.active and conflict:
+            result = 0xFF
+        else:
+            result = bus.readByte(address, obj = true)
+
     elif address.isboundto(0xFF00, 0xFF7F):
-        result = ioReadByte(address)
+        result = getIoReg((address and 0xFF).int)
 
     elif address.isboundto(0xFF80, 0xFFFE):
         result = hram[address - 0xFF80]
@@ -25,18 +77,32 @@ proc readByte*(address: uint16, incr = true): uint8 =
     elif address == 0xFFFF:
         result = IE
 
-    if incr:
-        incCycle(1)
+    cycle(incr)
 
 proc writeByte*(address: uint16, data: uint8, incr = true): void =
     if address.isboundto(0, 0x7FFF):
         rom.write(address, data)
 
+    elif address.isboundto(0x8000, 0x9FFF):
+        bus.writeByte(address, data)
+
+    elif address.isboundto(0xA000, 0xBFFF):
+        mockExternalRam[address - 0xA000] = data
+
     elif address.isboundto(0xC000, 0xDFFF):
         wram[address - 0xC000] = data
 
+    elif address.isboundto(0xE000, 0xFDFF):
+        wram[address - 0xE000] = data
+
+    elif address.isboundto(0xFE00, 0xFE9F):
+        bus.writeByte(address, data, obj = true)
+
     elif address.isboundto(0xFF00, 0xFF7F):
-        ioWriteByte(address, data)
+        setIoReg((address and 0xFF).int, data)
+        if address == 0xFF46:
+            dma.starting = true
+            dma.initialDelay = 2
 
     elif address.isboundto(0xFF80, 0xFFFE):
         hram[address - 0xFF80] = data
@@ -44,12 +110,26 @@ proc writeByte*(address: uint16, data: uint8, incr = true): void =
     elif address == 0xFFFF:
         IE = data
 
-    if incr:
-        incCycle(1)
+    cycle(incr)
 
 proc writeWord*(address, data: uint16): void =
     writeByte(address, data.lsb)
     writeByte(address + 1, data.msb)
 
 proc internal*(): void =
-    incCycle(1)
+    cycle()
+
+proc tick(d: DMA): void =
+    let address = io.DMA.uint16 shl 8
+    d.currentByte = readByte(address + d.currentIndex, incr = false,
+            conflict = false)
+
+    bus.writeByte(0xFE00 + d.currentIndex.uint16, d.currentByte, obj = true)
+    inc d.currentIndex
+
+    if d.currentIndex == 0xA0:
+
+        d.active = false
+        d.starting = false
+        d.initialDelay = 0
+        d.currentIndex = 0
--- src/emulator/io.nim
+++ src/emulator/io.nim
@@ -3,50 +3,73 @@ import bitops
 import utils
 import types
 
+
 var
     # Serial Data Buffer
-    serialData: array[2, uint8]
+    SB: uint8 = 0x00
+    SC: uint8 = 0x7E
 
     # Interrupt Registers
-    IE*: uint8 = 0x00
     IF*: uint8 = 0xE1
+    IE*: uint8 = 0x00
 
     # Timer Registers
-    DIV*: uint16 = 0x18
+    DIV*: uint16 = 0xAB
     TIMA*: uint8 = 0x00
     TMA*: uint8 = 0x00
     TAC*: uint8 = 0xF8
 
+    # PPU Registers
+    LCDC*: uint8 = 0x91
+    STAT*: uint8 = 0x85
+
+    SCY*: uint8 = 0x00
+    SCX*: uint8 = 0x00
+    LY*: uint8 = 0x00
+    LYC*: uint8 = 0x00
+    DMA*: uint8 = 0xFF
+    BGP*: uint8 = 0xFC
+    OBP0*: uint8 = 0x00
+    OBP1*: uint8 = 0x00
+    WY*: uint8 = 0x00
+    WX*: uint8 = 0x00
+
+
+# LUT to avoid condition chaining :/
+var
+    SerialRegTable: array[2, ptr uint8] = [addr SB, addr SC]
+    TimerRegTable: array[3, ptr uint8] = [addr TIMA, addr TMA, addr TAC]
+    PPURegTable: array[12, ptr uint8] = [addr LCDC, addr STAT, addr SCY,
+            addr SCX, addr LY, addr LYC, addr DMA, addr BGP, addr OBP0,
+            addr OBP1,
+            addr WY, addr WX]
+
+proc getIoReg*(address: int): uint8 =
+    if address.isboundto(0x01, 0x02):
+        result = SerialRegTable[address - 1][]
+
+    elif address == 0x0F: result = IF
+    elif address == 0x04: result = msb(DIV)
+
+    elif address.isboundto(0x05, 0x07):
+        result = TimerRegTable[address - 5][]
+
+    elif address.isboundto(0x40, 0x4B):
+        result = PPURegTable[address and 0xF][]
+
+proc setIoReg*(address: int, data: uint8): void =
+    if address.isboundto(0x01, 0x02):
+        SerialRegTable[address - 1][] = data
+
+    elif address == 0x0F: IF = data
+    elif address == 0x04: DIV = 0
+
+    elif address.isboundto(0x05, 0x07):
+        TimerRegTable[address - 5][] = data
+
+    elif address.isboundto(0x40, 0x4B):
+        PPURegTable[address and 0xF][] = data
 
 proc sendIntReq*(ISR: IntType): void =
     setBit(IF, ISR.ord)
 
-proc setTimerReg(reg: u2, data: uint8): void =
-    case reg
-    of 0: DIV = 0
-    of 1: TIMA = data
-    of 2: TMA = data
-    of 3: TAC = data
-
-proc getTimerReg(reg: u2): uint8 =
-    case reg
-    of 0: msb(DIV)
-    of 1: TIMA
-    of 2: TMA
-    of 3: TAC
-
-proc ioReadByte*(address: uint16): uint8 =
-    if address == 0xFF01: result = serialData[0]
-    elif address == 0xFF02: result = serialData[1]
-    elif address == 0xFF0F: result = IF
-    elif address.isboundto(0xFF04, 0xFF07):
-        result = getTimerReg(((address and 0xF) - 4).u2)
-    elif address.isboundto(0xFF40, 0xFF4B):
-        result = 0x90
-
-proc ioWriteByte*(address: uint16, data: uint8): void =
-    if address == 0xFF01: serialData[0] = data
-    elif address == 0xFF02: serialData[1] = data
-    elif address == 0xFF0F: IF = data
-    elif address.isboundto(0xFF04, 0xFF07):
-        setTimerReg(((address and 0xF) - 4).u2, data)
